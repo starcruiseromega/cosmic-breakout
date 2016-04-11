fe796df1e1e0a529ee6418e230917235fb5c8ced
Sally Wilsak <starcruiseromega@gmail.com>
2016-04-10 21:15:14 -0700

    Add the rest of the lid

--- case/make_lid.py
+++ case/make_lid.py
@@ -52,8 +52,46 @@ def draw_lid():
         (5, 1.6, 0),
     ]
     draw_wall(button_right_vertices, button_faces, vector, "ButtonRight")
+    midsection_vertices = [
+        (0, 1.6, 0),
+        (6, 1.6, 0),
+        (6, 3.2, 0),
+        (5, 3.2, 0),
+        (5, 4.2, 0),
+        (6, 4.2, 0),
+        (6, 4.5, 0),
+        (0, 4.5, 0),
+    ]
+    midsection_faces = [
+        (0, 1, 2, 3, 4, 5, 6, 7),
+    ]
+    draw_wall(midsection_vertices, midsection_faces, vector, "Midsection")
+    screen_left_vertices = [
+        (0, 4.5, 0),
+        (1, 4.5, 0),
+        (1, 7.2, 0),
+        (0, 7.2, 0),
+    ]
+    draw_wall(screen_left_vertices, button_faces, vector, "ScreenLeft")
+    screen_right_vertices = [
+        (4.8, 4.5, 0),
+        (6,   4.5, 0),
+        (6,   7.2, 0),
+        (4.8, 7.2, 0),
+    ]
+    draw_wall(screen_right_vertices, button_faces, vector, "ScreenRight")
+    top_vertices = [
+        (0,   7.2, 0),
+        (6,   7.2, 0),
+        (6,   7.4, 0),
+        (6.1, 7.4, 0),
+        (6.1, 8.4, 0),
+        (1,   8.4, 0),
+        (1,   8.5, 0),
+        (0,   8.5, 0),
+    ]
+    draw_wall(top_vertices, midsection_faces, vector, "Top")
  
-
 if __name__ == "__main__":
     delete_all()
     draw_lid()
